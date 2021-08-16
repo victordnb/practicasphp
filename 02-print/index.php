0f40e88f1e6054dc2e79cadc38608876a9c24042
Victor Cubiles <victordnb@gmail.com>
2021-08-16 22:30:22 +0200

    initial commit

---
+++ 02-print/index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE HTML>
+<html LANG="en">
+<head>
+    <meta charset="UTF-8"/>
+    <title>Printing for the Web</title>
+    <body>
+        <h1> Programando ejercicios en php </h1>
+        <?php
+            echo " <h3> Listado de videojuegos: </h3>";
+            echo "<ul>"
+            ."<li>GTA</li>"
+            ."<li>Fifa</li>"
+            ."<li>Pikachu</li>"
+            ."</ul>";
+
+            //strings with words listed
+            //using "." to concatenate strings
+            //using ul to list items
+            
+
+            echo "<h3> Listado de peliculas: </h3>";
+            echo "<ul>"
+            ."<li>El padrino</li>"
+            ."<li>El padrino 2</li>"
+            ."<li>El padrino 3</li>"
+            ."</ul>";
+        
+        echo '<p>Esta es toda'.' - '.'lista de peliculas'.'</p>';
+
+        
+            
+        ?>
+
+        <?="this is a shortcut to echo"?>
+    </body>
+</html>
+
+        
+
---
+++ 03-variables/index.php
@@ -0,0 +1,20 @@
+<?php
+$my_first_variable = "Hello World from a variable";
+
+echo '<h1>'.$my_first_variable.'</h1>';
+
+$num = 44;
+$boolval = true;
+
+// how asign a diferent data to other variable:
+
+$num = 77;
+
+echo '<h1>'.$num.'</h1>';
+
+
+
+
+
+
+?>
