83bbc5393976887da3d9a0563e896303fc9917b6
Victor Cubiles <victor.cubiles@novapago.com>
2021-09-06 13:31:14 +0200

    add css style

--- prBlog/index.php
+++ prBlog/index.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="utf-8">
     <title>Blog</title>
+    <link rel="stylesheet" type="text/css" href="./assets/css/style.css" >
 </head>
 <!-- body -->
 <body>
@@ -56,6 +57,29 @@
                 </form>
             </div>
         </aside>
+    
+        <!-- Main -->
+        <div id="main">
+            <h1>the last news</h1>
+            <article>
+                <h2>Title of entry</h2>
+                <p>Lorem ipsum dolor sit am
+                    amet, consectetur adipisicing elit, sed do eiusmod
+                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                    consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                    cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                    proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                </p>
+            </article>
+        </div>
+
+        <!-- footer -->
+        <footer id="footer">
+            <p>Copyright &copy; 2021</p>
+        </footer>
+
+                
 
 
 
