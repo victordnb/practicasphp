02f5075838fc6c7addb4e02d8f00ae5ff8fb3dc0
Victor Cubiles <victor.cubiles@novapago.com>
2021-09-06 13:00:27 +0200

    add html form login and register

--- prBlog/index.php
+++ prBlog/index.php
@@ -7,49 +7,55 @@
 <!-- body -->
 <body>
     <!-- header -->
-<header>
-    <a href="index.php">    
-    <h1>Blog</h1>
-    </a>    
-</header>
-<!-- main -->
-<main>
-    <?php
-        // if there is a post id in the url
-        if(isset($_GET['post_id'])){
-            // get the post id from the url
-            $post_id = $_GET['post_id'];
-            // get the post from the database
-            $post = getPost($post_id);
-            // if there is a post
-            if($post){
-                // display the post
-                displayPost($post);
-            }
-            // if there is no post
-            else{
-                // display error message
-                echo "<p>Post not found.</p>";
-            }
-        }
-        // if there is no post id in the url
-        else{
-            // get the posts from the database
-            $posts = getPosts();
-            // if there are posts
-            if($posts){
-                // display the posts
-                displayPosts($posts);
-            }
-            
+    <header id="header">
+        <!-- LOGO -->
+        <div id="logo">
+            <a href="index.php">
+                <img src="img/logo.png" alt="logo">
+                Blog of VideoGames
+            </a>
+        </div>
 
-    
+        <!-- Menu -->
+        <nav id="nav">
+            <ul>
+                <li><a href="index.php">Home</a></li>
+                <li><a href="about.php">About</a></li>
+                <li><a href="contact.php">Category 1</a></li>
+                <li><a href="contact.php">Category 2</a></li>
+                <li><a href="contact.php">Category 3</a></li>
+                <li><a href="contact.php">Category 4</a></li>
+                <li><a href="contact.php">Contact</a></li>
+            </ul>
+        </nav>
+    </header>
 
-
-<nav>
-
-</body>
-</html>
+    <!-- Aside -->
+    <div id="container">
+        <aside id="sidebar">
+            <div id="login" class="block-aside">
+                <h3>Login</h3>
+                <form action="login.php" method="post">
+                    <Label for="email">Email</Label>
+                    <input type="email" name="email" id="email" placeholder="Email">
+                    <Label for="password">Password</Label>
+                    <input type="password" name="password" id="password" placeholder="Password">
+                    <input type="submit" value="Login">
+                </form>
+            </div>
+            <div id="register" class="block-aside">
+                <h3>Register</h3>
+                <form action="register.php" method="post">
+                    <Label for="name">Name</Label>
+                    <input type="text" name="name" id="name" placeholder="Name">
+                    <Label for="email">Email</Label>
+                    <input type="email" name="email" id="email" placeholder="Email">
+                    <Label for="password">Password</Label>
+                    <input type="password" name="password" id="password" placeholder="Password">
+                    <input type="submit" value="Register">
+                </form>
+            </div>
+        </aside>
 
 
 
